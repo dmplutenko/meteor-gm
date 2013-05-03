ff685eee9cff91b65964639a02b48bb26e700d08
dm <dmplutenko@gmail.com>
2013-05-03 04:54:58 +0400

    v0.0.7

--- gm_server.js
+++ gm_server.js
@@ -1,60 +1,68 @@
-GM =(function(){
-  
-  var installNodeJsPackage=function(meteorPackage,nodePackage){
-    var require=__meteor_bootstrap__.require;
-    var path = require('path');
-    var child_process = require('child_process');
-    var future = require ('fibers/future');
-    var spawnSync = function(file, args, options) {
-      var wrapped = future.wrap(function(cb) {
-        var proc = child_process.spawn(file, args, options);
-        proc.on('close', function(code, signal) {
-          cb(code !== 0 ? "Command failed with exit code " + code : null);
-        });
-        proc.on('error', function(error) {
-          cb(error);
-        });
+GM = (function(meteorPackage,nodePackage){
+  var require=__meteor_bootstrap__.require;
+  var path = require('path');
+  var child_process = require('child_process');
+  var future = require ('fibers/future');
+  var spawnSync = function(file, args, options) {
+    var wrapped = future.wrap(function(cb) {
+      var proc= child_process.spawn(file, args, options);
+      proc.on('close', function(code, signal) {
+        cb(code !== 0 ? "Command failed with exit code " + code : null);
       });
-      wrapped().wait();
-    };
-    packagePath=path.join(process.env.PACKAGE_DIRS, meteorPackage);
-    var pac;
+      proc.on('error', function(error) {
+        cb(error);
+      });
+    });
+    wrapped().wait();
+  };
+  var pac;
+  try{
+    pac=require(nodePackage);
+  }catch(e){
     try{
-      pac=require(nodePackage);
-    }catch(e){
+      var packagePath=path.join(process.env.PACKAGE_DIRS, meteorPackage);
       spawnSync("npm",["install",nodePackage],{cwd:packagePath,stdio:'inherit'});
       pac=require(nodePackage); 
-    }
-    return pac;
-  }
-
-  var installNodeJsPackageInDir=function(meteorPackage,nodeDir){
-    var require=__meteor_bootstrap__.require;
-    var path = require('path');
-    var child_process = require('child_process');
-    var future = require ('fibers/future');
-    var spawnSync = function(file, args, options) {
-      var wrapped = future.wrap(function(cb) {
-        var proc = child_process.spawn(file, args, options);
-        proc.on('close', function(code, signal) {
-          cb(code !== 0 ? "Command failed with exit code " + code : null);
-        });
-        proc.on('error', function(error) {
-          cb(error);
-        });
-      });
-      wrapped().wait();
-    };
-    packagePath=path.join(process.env.PACKAGE_DIRS, meteorPackage,nodeDir);
-    var pac;
-    try{
+    }catch(e2){
+      console.log("Install node package "+nodePackage+" on heroku...");
+      var fs=require('fs');
+      var smartLock=JSON.parse(fs.readFileSync(path.join(process.env.PWD,'smart.lock')) );
+      
+      var binPath=path.join(process.env.PWD,'.meteorite/meteors/meteor/meteor',smartLock.meteor.commit,'dev_bundle/bin')
+      var npmPath=path.join(binPath,'npm');
+      var packagePath=path.join(binPath,'node_modules',nodePackage);
+      
+      spawnSync(npmPath,["install",nodePackage],{cwd:binPath,stdio:'inherit'});
       pac=require(packagePath);
-    }catch(e){
-      spawnSync("npm",["install"],{cwd:packagePath,stdio:'inherit'});
-      pac=require(packagePath); 
     }
-    return pac;
   }
-  
-  return installNodeJsPackage('gm', 'gm');
-})();
+  return pac;
+})('gm', 'gm');
+
+/*var installNodeJsPackageInDir=function(meteorPackage,nodeDir){
+  var require=__meteor_bootstrap__.require;
+  var path = require('path');
+  var child_process = require('child_process');
+  var future = require ('fibers/future');
+  var spawnSync = function(file, args, options) {
+    var wrapped = future.wrap(function(cb) {
+      var proc = child_process.spawn(file, args, options);
+      proc.on('close', function(code, signal) {
+        cb(code !== 0 ? "Command failed with exit code " + code : null);
+      });
+      proc.on('error', function(error) {
+        cb(error);
+      });
+    });
+    wrapped().wait();
+  };
+  packagePath=path.join(process.env.PACKAGE_DIRS, meteorPackage,nodeDir);
+  var pac;
+  try{
+    pac=require(packagePath);
+  }catch(e){
+    spawnSync("npm",["install"],{cwd:packagePath,stdio:'inherit'});
+    pac=require(packagePath); 
+  }
+  return pac;
+}*/
